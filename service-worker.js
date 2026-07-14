a5cfa20ee80123e9aca766304b7ca165e2f4d33f
onenine-cell <kopeczylogan43@gmail.com>
2026-07-14 15:48:51 -0700

    Update cache version from v1 to v2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'money-moves-v1';
+const CACHE = 'money-moves-v2';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
