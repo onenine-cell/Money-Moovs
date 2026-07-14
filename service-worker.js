88c7ad77ff663025d16ffdc31d93b8a356c96878
onenine-cell <kopeczylogan43@gmail.com>
2026-07-14 16:03:45 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const CACHE = 'money-moves-v2';
+const CACHE = 'money-moves-v4';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
+
