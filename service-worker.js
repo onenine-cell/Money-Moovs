9c6aece06e9b727b1b9a63890bc018f7c8f2ece2
onenine-cell <kopeczylogan43@gmail.com>
2026-07-15 04:00:01 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'money-moves-v10';
+const CACHE = 'money-moves-v11';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
